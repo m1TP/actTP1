26e017c9c7b4ea1f4ec61474c743323d377b8087
Julien Rouse <julien.rouse@gmail.com>
2015-09-26 14:54:56 +0200

    Fixed bad class naming after refactoring

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,8 +3,9 @@ package main;
 import java.util.ArrayList;
 import java.util.List;
 
+import util.IO;
 import util.Point;
-import util.SVG;
+
 
 public class Main {
 
@@ -17,8 +18,8 @@ public class Main {
 		for(int i=2;i<7;i++)
 			l.add(new Point(i,(int)Math.pow(-2, i)));
 		
-		String svg = SVG.toSVGFormat(l);
-		SVG.writeToSVG(svg);
+		String svg = IO.toSVGFormat(l);
+		IO.writeToSVG(svg);
 
 	}
 
