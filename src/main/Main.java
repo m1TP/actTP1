274c0c535358f85827f5994f7dbc37cd3267cdd4
JulienRouse <julien.rouse@gmail.com>
2015-10-23 08:08:39 +0200

    save

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,25 +1,34 @@
 package main;
 
+import implementation.Exo6;
+
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+import util.Building;
 import util.Point;
-import util.SVG;
+import util.IO;
 
 public class Main {
 
-	/**
-	 * @param args
-	 */
+	
 	public static void main(String[] args) {
 		
+		/*
 		List<Point> l = new ArrayList<Point>();
+		 
 		for(int i=2;i<7;i++)
 			l.add(new Point(i,(int)Math.pow(-2, i)));
 		
 		String svg = SVG.toSVGFormat(l);
 		SVG.writeToSVG(svg);
-
+		*/
+		
+		List<Building> l = IO.readBuilding(new File("building.txt"));
+		
+		Exo6.solve(l);
+		
 	}
 
 }
